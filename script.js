2e4623d5a6a3a253dcea74a6028608d7174800b2
Omolara5861 <debbiegterra@gmail.com>
2023-12-04 09:23:57 +0100

    refactor: :recycle: Update script

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ const header = document.querySelector("[data-header]");
 const backTopBtn = document.querySelector("[data-back-top-btn]");
 
 window.addEventListener("scroll", function () {
-  if (window.scrollY >= 60) {
+  if (window.scrollY >= 50) {
     header.classList.add("active");
     backTopBtn.classList.add("active");
   } else {
